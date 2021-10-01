4dac900e0aa29a85a63cce6c98a3a66181af476e
Aman Panesar <amanpanesar@Amans-MacBook-Pro.local>
2021-10-01 16:29:31 +0100

    rolls games of ones and zeros

--- src/main/java/com/codurance/amanmattsi/Game.java
+++ src/main/java/com/codurance/amanmattsi/Game.java
@@ -1,7 +1,15 @@
 package com.codurance.amanmattsi;
 
+import java.util.stream.IntStream;
+
 public class Game {
     public int calculateGameScore(String gameNotation) {
-        return 0;
+        String[] formattedGameNotation = gameNotation.split("\\|");
+        int score = 0;
+        for (String frame : formattedGameNotation) {
+            char[] pins = frame.toCharArray();
+            score += Character.getNumericValue(pins[0]) + Character.getNumericValue(pins[1]);
+        }
+        return score;
     }
 }
--- src/test/java/com/codurance/amanmattsi/BowlingGameCalculatorShould.java
+++ src/test/java/com/codurance/amanmattsi/BowlingGameCalculatorShould.java
@@ -1,5 +1,6 @@
 package com.codurance.amanmattsi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,16 +9,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class BowlingGameCalculatorShould {
 
-	@Test
-	void create_a_game() {
-		Game g = new Game();
+	private Game game;
+
+	@BeforeEach
+	void setUp() {
+		game = new Game();
 	}
 
 	@Test
 	void roll_a_game_of_0() {
-		Game g = new Game();
-		int calculatedScore = g.calculateGameScore("00|00|00|00|00|00|00|00|00|00");
+		int calculatedScore = game.calculateGameScore("00|00|00|00|00|00|00|00|00|00");
 
 		assertEquals(0, calculatedScore);
+	}
+
+	@Test
+	void roll_a_game_of_all_ones() {
+		int calculatedScore = game.calculateGameScore("11|11|11|11|11|11|11|11|11|11");
+
+		assertEquals(20, calculatedScore);
 	}
 }
